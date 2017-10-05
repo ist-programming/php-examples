81804adbf03af1d84c56ceb50eb391ac0a49c207
Alexander Ferenets <ist.kazan@gmail.com>
2017-10-05 13:56:58 +0300

    Magic __set and __get methods example

---
+++ OOP/MagicSetGet_Example.php
@@ -0,0 +1,38 @@
+<?php
+
+class A
+{
+
+  public $a = 'Default value';
+  private $b = 'Default value';
+
+  public function __set($name, $value)
+  {
+    echo "Tried to set " . $name . ' = ' . $value . "\n";
+  }
+
+  public function __get($name)
+  {
+    echo 'Tried to get ' . $name . "\n";
+  }
+
+}
+
+echo '<pre>';
+echo "\n\nInit object\n";
+$a = new A();
+var_dump($a);
+echo "\n\nSet a value\n";
+$a->a = "A value";
+var_dump($a);
+echo "\n\nSet b value\n";
+$a->b = "B value";
+var_dump($a);
+echo "\n\nSet c value\n";
+$a->c = "C value";
+var_dump($a);
+
+echo "\n\nTry to set new dynamic field to object of StdClass\n";
+$a = new StdClass();
+$a->a = 42;
+var_dump($a);
